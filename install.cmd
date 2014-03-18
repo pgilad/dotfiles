fdd921b98ea9510c6ec2cbba667399df1816e735
Gilad Peleg <giladp007@gmail.com>
2014-03-18 18:10:23 +0200

    Add gitconfig and update install

--- install.cmd
+++ install.cmd
@@ -4,6 +4,7 @@
 @set APP_DIR=%HOME%\.dotfiles
 
 call mklink "%HOME%\_vimrc" "%APP_DIR%\_vimrc"
+call mklink "%HOME%\.gitconfig" "%APP_DIR%\.gitconfig"
 
 IF NOT EXIST "%APP_DIR%\vimfiles\bundle" (
 	call mkdir "%APP_DIR%\.vim\bundle"
