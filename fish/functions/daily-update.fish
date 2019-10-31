7c05d54c46c5218ae20680c3049717bb72a793d4
pgilad <gilad.peleg@forter.com>
2019-10-31 22:26:46 +0200

    WIP

--- fish/functions/daily-update.fish
+++ fish/functions/daily-update.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/0c/_14xdpfn3b9dw81ndwyx9zjm0000gn/T//fish.7jdKyY/daily-update.fish @ line 2
+# Defined in /Users/giladpeleg/.config/fish/functions/daily-update.fish @ line 2
 function daily-update --description 'Keep Mac up-to date'
 	if not type -q cowsay
         echo "Please install cowsay"
@@ -8,14 +8,18 @@ function daily-update --description 'Keep Mac up-to date'
         echo "Please install lolcat"
         return 1
     end
-    __daily-update-echo "Updating OSX"
+
+    function __echo-phase
+        cowsay $argv[1] | lolcat
+    end
+    __echo-phase "Updating OSX"
     sudo softwareupdate --install --all
-    __daily-update-echo "Updating Brew"
+    __echo-phase "Updating Brew"
     brew update; brew upgrade; brew cleanup; brew doctor
-    __daily-update-echo "Updating SDK"
+    __echo-phase "Updating SDK"
     sdk install gradle; and sdk install maven; and sdk install groovy
-    __daily-update-echo "Updating Node.js"
+    __echo-phase "Updating Node.js"
     nvm install node
-    __daily-update-echo "Updating Fisher"
+    __echo-phase "Updating Fisher"
     fisher; and fisher self-update
 end
