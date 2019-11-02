3946d5ff1f6cd349f5accf53a7375942de36d1bb
pgilad <gilad.peleg@forter.com>
2019-11-02 07:32:40 +0200

    Add verifying to brewfile

--- fish/functions/daily-update.fish
+++ fish/functions/daily-update.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/0c/_14xdpfn3b9dw81ndwyx9zjm0000gn/T//fish.ZfBEF0/daily-update.fish @ line 2
+# Defined in /var/folders/0c/_14xdpfn3b9dw81ndwyx9zjm0000gn/T//fish.OjCsgw/daily-update.fish @ line 2
 function daily-update --description 'Keep Mac up-to date'
 	if not type -q cowsay
         echo "Please install cowsay"
@@ -27,6 +27,9 @@ function daily-update --description 'Keep Mac up-to date'
 
     __echo-phase "Updating Fisher"
     fisher; and fisher self-update
+
+    __echo-phase "Making sure Brewfile is up-to-date"
+    brew bundle check --verbose --file="$XDG_CONFIG_HOME/brew/Brewfile"
 
     __echo-phase "Updating Fish Completions"
     fish_update_completions
