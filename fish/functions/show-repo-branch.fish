07a4468419d2bb162a4cf4ada29ef0ee8573e481
pgilad <gilad@giladpeleg.com>
2019-11-27 07:43:34 +0200

    Update show repo branch

--- fish/functions/show-repo-branch.fish
+++ fish/functions/show-repo-branch.fish
@@ -1,20 +1,31 @@
 function show-repo-branch --description 'Show repository branches'
-    pushd (pwd)
-    cd ~/dev
-    set -l master_branches master develop
-    for i in */
-        cd $i
-        set branch (git branch --show-current)
-        set repo (string trim -c / $i)
+    set dev_dir ~/dev
+    set trunk_branches master develop
+
+    if not test -d $dev_dir
+        echo "Development directory: $dev_dir not found"
+        return
+    end
+
+    for i in $dev_dir/*/
+        set repo (basename $i)
+        set full_path "$dev_dir/$repo"
+
         echo -n "$repo: "
-        if contains $branch $master_branches
+
+        if not test -d "$full_path/.git"
+            echo "Not a git directory"
+            continue
+        end
+
+        set branch (git -C $full_path branch --show-current)
+        if contains $branch $trunk_branches
             set_color green
             echo "$branch"
-            set_color normal
         else
+            set_color red
             echo "$branch"
         end
-        cd ..
+        set_color normal
     end
-    popd
 end
