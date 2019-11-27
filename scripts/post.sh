2f0695975133909324762420492ba12a116b3d78
pgilad <gilad@giladpeleg.com>
2019-11-27 23:19:41 +0200

    Refactor post script

---
+++ scripts/post.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+git submodule --quiet update --init --recursive
+chown -R "$(whoami)" ~/.gnupg
+find ~/.gnupg -type f -exec chmod 600 {} \;
+find ~/.gnupg -type d -exec chmod 700 {} \;
+chmod 700 ~/.ssh
+chmod 644 ~/.ssh/config
+chmod 644 ~/.ssh/config.d/*
+
+if command -v pipx; then
+    pipx install pipenv
+    pipx install poetry --pip-args="--pre"
+fi
+
+if command -v vim; then
+    vim -c NeoBundleInstall -c NeoBundleUpdate -c q
+fi
+
+if test uname -s | grep --quiet --ignore-case darwin; then
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm2"
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
