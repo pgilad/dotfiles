c7d2e206fe3b2f65f3ec2861c8e474e26c94484f
pgilad <gilad.peleg@forter.com>
2019-11-28 12:59:26 +0200

    WIP

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTBOT_BIN_FULL_PATH="${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}"
 
-# Null this in order to have verbose install
-QUIET_INSTALL="--quiet"
+DOTBOT_FLAGS=()
+
+# Running in Github actions
+if [[ "${GITHUB_ACTIONS:-}" != "true" ]]; then
+    DOTBOT_FLAGS+=("--quiet")
+fi
 
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule --quiet sync --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
-"$DOTBOT_BIN_FULL_PATH" $QUIET_INSTALL --base-directory "${BASEDIR}" --config-file "${CONFIG}" "${@}"
+"$DOTBOT_BIN_FULL_PATH" "${DOTBOT_FLAGS[@]}" --base-directory "${BASEDIR}" --config-file "${CONFIG}" "${@}"
