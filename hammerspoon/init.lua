aa0cd4765d1e0ce03002384fc8df601ffabcd500
pgilad <gilad.peleg@forter.com>
2019-11-30 16:26:49 +0200

    Minor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,15 +3,12 @@ local log = hs.logger.new('hammerspoon','debug')
 
 function openApp(name)
     app = hs.application.find(name)
-    log.i("open app", name, app)
 
     if app and app:isFrontmost() then
-        log.i("hiding app")
         app:hide()
         return
     end
 
-    log.i("launching app")
     hs.application.launchOrFocus(name)
 end
 
@@ -26,12 +23,12 @@ hs.hotkey.bind(hyper, 'i', mountApp("iTerm"))
 hs.hotkey.bind(hyper, 'j', mountApp("IntelliJ IDEA"))
 hs.hotkey.bind(hyper, 's', mountApp("Slack"))
 
-hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
+hs.hotkey.bind(hyper, "r", function() hs.reload() end)
 
 -- local wifiMenu = hs.menubar.newWithPriority(2147483645)
 
 -- function ssidChanged()
-    -- wifiMenu:setTitle(hs.wifi.currentNetwork() or "Offline")
+-- wifiMenu:setTitle(hs.wifi.currentNetwork() or "Offline")
 -- end
 
 -- wifiWatcher = hs.wifi.watcher.new(ssidChanged):start()
