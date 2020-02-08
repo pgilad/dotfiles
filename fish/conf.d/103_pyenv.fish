35b2ede2084e4b6320bf78efb0765ab51c1b0e82
pgilad <gilad.peleg@forter.com>
2020-02-08 21:49:12 +0200

    WIP

--- fish/conf.d/103_pyenv.fish
+++ fish/conf.d/103_pyenv.fish
@@ -1,5 +1,5 @@
 # Pyenv
-if status --is-interactive; and type -q pyenv
+if status --is-interactive; and type --quiet pyenv; and not set --query VIRTUAL_ENV
     source (pyenv init - | psub)
     source (pyenv virtualenv-init - | psub)
 end
