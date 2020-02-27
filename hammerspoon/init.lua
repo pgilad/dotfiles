4e4e2d8cdb7adb1a1a1454d34041b80d434d7f66
pgilad <gilad.peleg@forter.com>
2020-02-27 20:06:34 +0200

    WIP

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -18,6 +18,25 @@ function mountApp(appName)
     end
 end
 
+hs.hotkey.bind(hyper, 'n', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect 
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
+
+-- Disable animations
+hs.window.animationDuration = 0
+
+hs.hotkey.bind(hyper, "f", function()
+  	local win = hs.window.focusedWindow();
+  	if not win then return end
+	win:moveToUnit(hs.layout.maximized)
+end)
+
 hs.hotkey.bind(hyper, 'c', mountApp("Google Chrome"))
 -- hs.hotkey.bind(hyper, 'i', mountApp("iTerm"))
 hs.hotkey.bind(hyper, 'j', mountApp("IntelliJ IDEA"))
@@ -25,6 +44,19 @@ hs.hotkey.bind(hyper, 's', mountApp("Slack"))
 hs.hotkey.bind(hyper, 't', mountApp("Alacritty"))
 
 hs.hotkey.bind(hyper, "r", function() hs.reload() end)
+
+-- resizing window
+hs.hotkey.bind(hyper, "Left", function()
+  	local win = hs.window.focusedWindow();
+  	if not win then return end
+	win:moveToUnit(hs.layout.left50)
+end)
+
+hs.hotkey.bind(hyper, "Right", function()
+  	local win = hs.window.focusedWindow();
+  	if not win then return end
+	win:moveToUnit(hs.layout.right50)
+end)
 
 -- local wifiMenu = hs.menubar.newWithPriority(2147483645)
 
--- vim/.vimrc
+++ vim/.vimrc
@@ -31,6 +31,7 @@ let g:dein#auto_recache = 1
 let g:dein#install_progress_type = 'title'
 let g:dein#enable_notification = 1
 let g:dein#install_log_filename = '~/dein.log'
+let g:dein#install_progress_type = 'tabline'
 
 execute "set runtimepath^=" . s:dein_plugin_path
 
