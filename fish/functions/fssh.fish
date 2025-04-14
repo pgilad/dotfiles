67f2a834049aed0045163c38b79cb00eda434384
pgilad <giladp007@gmail.com>
2025-04-14 09:30:33 +0300

    WIP

--- fish/functions/fssh.fish
+++ fish/functions/fssh.fish
@@ -1,3 +1,6 @@
-function fssh --description 'Forter SSH shortcut' --argument-names target_ip --wraps ssh
-    command ssh -F ~/.ssh/gozer-new "gilad.peleg@$target_ip"
+function fssh --description 'Forter SSH shortcut' --argument-names target_ip username --wraps ssh
+    if test -z "$username"
+        set username "gilad.peleg"
+    end
+    command ssh -F ~/.ssh/gozer-new "$username@$target_ip"
 end
